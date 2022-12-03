f43afefa90711e418e0ee1d2c0f8707430e32554
kotlyarevskiy <48545383+kotlyarevskiy@users.noreply.github.com>
2022-12-03 17:47:20 +0200

    Update Practice_01_01.py

--- Practice_01_01.py
+++ Practice_01_01.py
@@ -1,3 +1,6 @@
+# Practical lesson No. 1, task No. 1.
+# This program displays the data entered by the user on the screen.
+
 name, middle_name, lastname = \
     str(input("Enter your name: ")), \
     str(input("Enter your middle name: ")), \
