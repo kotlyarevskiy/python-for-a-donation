948f8f820e8c7ec1f27c77ac33a2fe575bb304aa
kotlyarevskiy <48545383+kotlyarevskiy@users.noreply.github.com>
2022-12-03 17:44:08 +0200

    Add files via upload

---
+++ Practice_01_01.py
@@ -0,0 +1,6 @@
+name, middle_name, lastname = \
+    str(input("Enter your name: ")), \
+    str(input("Enter your middle name: ")), \
+    str(input("Enter your last name: "))
+
+print("Hello, " + name + " " + middle_name + " " + lastname + "!")
