dcdd28c6821369c09780e82463b91f2b8ebeb33c
kotlyarevskiy <48545383+kotlyarevskiy@users.noreply.github.com>
2022-12-03 18:10:05 +0200

    Add files via upload

---
+++ Practice_01_02.py
@@ -0,0 +1,10 @@
+# Practical lesson No. 1, task No. 2.
+# This program determines the number of characters in a line that the user types
+# and prohibits input of lines that are longer than 20 characters.
+
+input_string = str(input("Enter a string that is no more than 20 characters long: "))
+string_len = len(input_string)
+if string_len > 20:
+    print(f"WARNING! You have entered more than 20 characters ({string_len})!!!")
+else:
+    print(input_string)
